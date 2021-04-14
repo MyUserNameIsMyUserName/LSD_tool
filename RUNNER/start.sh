3790156ea96f2b62208906bef1b9f704f23e02c7
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2021-04-14 05:00:55 +0200

    yea...both inits...some demo start point for actually working with this

---
+++ RUNNER/start.sh
@@ -0,0 +1,17 @@
+echo "I WILL START THIS [ FILE :> SERVER->start.sh ]"
+echo "Sleepy Time ZZzzZZzz....will wait to see this msg for 15s"
+echo -n "[__.(0s)..>"
+sleep 5
+echo -n "..(5s)..>"
+sleep 5
+echo -n "..(10s)..>"
+sleep 5
+echo -n "..(15s)..>"
+sleep 5
+echo -n "..(20s).__]"
+echo ""
+echo ""
+echo "+---------------+"
+echo "+ ByeBye...     +"
+echo "+---------------+"
+sleep 1
